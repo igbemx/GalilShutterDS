dd29cf6c4e27881fbe785bda5275f3fe232a8f76
Igor Beinik <igor.beinik@maxiv.lu.se>
2021-10-07 16:52:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3 
 from setuptools import setup, find_packages
 setup(name = "GalilShutterDS",
-    version = "1.0.2",
+    version = "1.0.3",
     description = "Device server for the Galil motion controller based shutter at Softimax",
     author = "Igor Beinik",
     author_email = "igor.beinik@maxiv.lu.se",
