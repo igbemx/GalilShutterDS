35ea93c9f73b6d4f2c41fdab73bb1e4f6d0a20b7
Igor Beinik <igor.beinik@maxiv.lu.se>
2022-03-11 15:55:40 +0100

    Change folder structure

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3 
-from setuptools import setup, find_packages
-setup(name = "GalilShutterDS",
-    version = "1.0.9",
-    description = "Device server for the Galil motion controller based shutter at Softimax",
-    author = "Igor Beinik",
-    author_email = "igor.beinik@maxiv.lu.se",
-    license = "GPLv3",
-    url = "http://www.maxiv.lu.se",
-    packages = find_packages(),
-    install_requires = ['pytango',],
-    entry_points={'console_scripts': ['SoftiGalilShutterDS = GalilShutter.GalilShutterDS:main',],}
-    )
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GalilShutterDS",
+    version="1.0.10",
+    description="Device server for the Galil motion controller based shutter at Softimax",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Igor Beinik",
+    author_email="igor.beinik@maxiv.lu.se",
+    license="GPLv3",
+    url="https://gitlab.maxiv.lu.se/igobei/tangods-softimax-galilshutter",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=['pytango', ],
+    entry_points={
+        'console_scripts': [
+            'SoftiGalilShutterDS = GalilShutter.GalilShutterDS:main',
+        ],
+    },
+)
